0b0da00f956445dbdcdf0b6aa2d6cf31ba74eadd
J0hn8uff3r <bufferoverflow@hotmail.es>
2020-10-04 17:25:24 +0200

    More code moved to functions

--- lastfm-now-playing_telegram_share.py
+++ lastfm-now-playing_telegram_share.py
@@ -9,23 +9,17 @@ import os
 userName = ""
 apiKey = ""
 
-minutes_to_wait_until_set_original_telegram_name = 20
-
 # Global Variables
-currentTrackURL = ('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&nowplaying="true"&user=' +
-                   str(userName) + '&api_key=' + str(apiKey))
-runCheck = True
-waitTime = 1
+apiTrackURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&nowplaying="true"&user=' + \
+    userName + '&api_key=' + apiKey
 
-# global start
+
 currentShowedSong = currentSongInfo = diskCover = ""
-# global start
-# song = ""
-
-
 start = perf_counter()
 http = PoolManager(maxsize=10)
 app = Client("lastfm-now-playing_telegram-share")
+minutes_to_wait_until_set_original_telegram_name = 20
+waitTime = 1
 
 
 def elapsed_minutes():
@@ -39,28 +33,73 @@ def restore_original_name():
         with app:
             app.update_profile(first_name=original_name)
             print("Name restored to: " + original_name)
-    except:
+    except Exception as e:
+        print(e)
         pass
 
 
+def remove_first_profile_photo():
+    try:
+        with app:
+            app.delete_profile_photos(app.get_profile_photos("me")[0].file_id)
+            print("Removed first profile picture")
+    except Exception as e:
+        print(e)
+        pass
+
+
+def set_first_profile_photo(newdiskCover):
+    global file
+    newdiskCover = newdiskCover.replace("300x300", "1200x1200")
+    print(newdiskCover)
+    os.system("curl -k -o cover_lastfm.jpg " + newdiskCover)
+    newdiskCover = newdiskCover.replace("1200x1200", "300x300")
+    diskCover = newdiskCover
+    print(newdiskCover)
+    with app:
+        app.set_profile_photo(photo="cover_lastfm.jpg")
+        print("Profile picture changed to: " + diskCover)
+    file.seek(0)
+    file.write(newdiskCover)
+    file.truncate()
+
+
+def profile_name_changer(currentSongInfo):
+    with app:
+        app.update_profile(first_name='🎶'+currentSongInfo+'🎶')
+        print("Name changed to: " + '🎶'+currentSongInfo+'🎶')
+
+
 def last_cover_checker(newdiskCover):
-    with open("last_cover_url.txt", "r+") as file:
-        last_cover_url = file.readline()
-        print("last_cover_url: " + last_cover_url)
-        print("new_cover_url: " + newdiskCover)
-        if last_cover_url != newdiskCover:
-            newdiskCover = newdiskCover.replace("300x300", "1200x1200")
-            print(newdiskCover)
-            os.system("curl -k -o cover_lastfm.jpg " + newdiskCover)
-            newdiskCover = newdiskCover.replace("1200x1200", "300x300")
-            diskCover = newdiskCover
-            print(newdiskCover)
-            with app:
-                app.set_profile_photo(photo="cover_lastfm.jpg")
-                print("Profile picture changed to: " + diskCover)
-            file.seek(0)
-            file.write(newdiskCover)
-            file.truncate()
+    if newdiskCover != "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png":
+        with open("last_cover_url.txt", "r+") as file:
+            last_cover_url = file.readline()
+            print("Last_cover_url: " + last_cover_url)
+            print("New_cover_url: " + newdiskCover)
+            if last_cover_url != newdiskCover:
+                set_first_profile_photo(newdiskCover)
+
+
+def reset_time_counter():
+    global start
+    start = perf_counter()
+
+
+def get_album_information(apiTrackURL):
+    global currentTrack
+    currentTrackXML = http.request('GET', apiTrackURL).data
+    currentTrack = minidom.parseString(currentTrackXML)
+    songName = currentTrack.getElementsByTagName('name')
+    songArtist = currentTrack.getElementsByTagName('artist')
+    currentSongInfo = songName[0].firstChild.nodeValue + \
+        " - " + songArtist[0].firstChild.nodeValue
+    return currentSongInfo
+
+
+def get_album_cover():
+    newdiskCover = currentTrack.getElementsByTagName(
+        'image')[3].firstChild.nodeValue
+    return newdiskCover
 
 
 def checkForNewSong():
@@ -68,43 +107,41 @@ def checkForNewSong():
     global minutes_to_wait_until_set_original_telegram_name
     global app
     global diskCover
-    global start
     global original_name
     global currentSongInfo
     # Loads Current Song info from Last FM
     try:
-        currentTrackXML = http.request('GET', currentTrackURL).data
-        currentTrack = minidom.parseString(currentTrackXML)
-        songName = currentTrack.getElementsByTagName('name')
-        songArtist = currentTrack.getElementsByTagName('artist')
-        currentSongInfo = songName[0].firstChild.nodeValue + \
-            " - " + songArtist[0].firstChild.nodeValue
+        currentSongInfo = get_album_information(apiTrackURL)
     except Exception as e:
         print(e)
         pass
 
     if currentShowedSong != currentSongInfo:
-        start = perf_counter()
-        newdiskCover = currentTrack.getElementsByTagName(
-            'image')[3].firstChild.nodeValue
-        currentShowedSong = currentSongInfo
-        # song = currentSongInfo
+        try:
+            reset_time_counter()
+            newdiskCover = get_album_cover()
+            currentShowedSong = currentSongInfo
 
-        if newdiskCover != diskCover:
-            last_cover_checker(newdiskCover)
+            profile_name_changer(currentSongInfo)
 
-        with app:
-            app.update_profile(first_name='🎶'+currentSongInfo+'🎶')
-            print("Name changed to: " + '🎶'+currentSongInfo+'🎶')
+            if newdiskCover != diskCover:
+                last_cover_checker(newdiskCover)
+
+        except Exception as e:
+            print(e)
+            pass
 
     if elapsed_minutes() >= minutes_to_wait_until_set_original_telegram_name:
-        # global start
-        start = perf_counter()
-        with app:
-            current_name = app.get_users("me").first_name
-            if current_name != original_name:
-                restore_original_name()
-                original_name = current_name
+        reset_time_counter()
+        try:
+            with app:
+                current_name = app.get_users("me").first_name
+                if current_name != original_name:
+                    restore_original_name()
+                    original_name = current_name
+        except Exception as e:
+            print(e)
+            pass
     sleep(waitTime)
 
 
@@ -119,6 +156,7 @@ if __name__ == '__main__':
             print('KeyboardInterruption')
             try:
                 restore_original_name()
+                remove_first_profile_photo()
                 print("TECLADO")
                 sys.exit(0)
             except SystemExit:
